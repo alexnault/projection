3e907ba7661c47bee0b8d2be3b1e3b886c880dd4
Alex Nault <nault.alex@gmail.com>
2015-05-19 20:50:11 -0400

    Add istanbul dependency & configure coveralls

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,19 @@
 'use strict';
 
-var app = require('../lib-cov/projection');
+var g =  (process.env['COVERAGE'] == 1);
+
+var libpath = g ? '../lib-cov/' : '../lib/';
+//var something = require(libpath + '/something');
+//var other = require(libpath + '/other');
+
+var app = require(libpath + 'projection');
+var assert = require("assert")
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
