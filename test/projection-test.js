957b8ad5269c5ad28d94691497ba4a3519654cf4
Alex Nault <nault.alex@gmail.com>
2015-05-27 18:28:04 -0400

    Convert remaining tabs to spaces

--- test/projection-test.js
+++ test/projection-test.js
@@ -5,64 +5,56 @@ var projection = require('../lib/projection');
 
 var p; // Projection
 
-// Expected JSON format
-// [
-// 	{ name: "...", movies: [ {}, {} ]},
-// 	{ name: "...", movies: [ {}, {} ]}
-// ]
-
 describe('Projection', function(){
-	beforeEach(function(){
-	  p = projection();
-	})
-
-	describe('new Projection()', function(){
-    it('should have the corresponding endpoint', function(){
-    	assert.equal(p.GOOGLE_ENDPOINT, 'http://www.google.com/movies');
-    })
+  beforeEach(function(){
+    p = projection();
   })
 
-  describe('#findTheaters()', function(){
+  it('should have the corresponding endpoint', function(){
+    assert.equal(p.GOOGLE_ENDPOINT, 'http://www.google.com/movies');
+  })
+
+  describe('.findTheaters()', function(){
     it('should find theaters by town', function(done){
-			p.findTheaters('Montreal', {}, function(err, theaters) {
-				//console.log(theaters);
+      p.findTheaters('Montreal', {}, function(err, theaters) {
+        //console.log(theaters);
 
-				assert.equal(err, null);
-				assert(theaters.length > 0);
-				assert(theaters[0].name);
+        assert.equal(err, null);
+        assert(theaters.length > 0);
+        assert(theaters[0].name);
 
-				done();
-			});
+        done();
+      });
     })
 
     it('should find theaters by zipcode', function(done){
-    	p.findTheaters('Montreal', {}, function(err, theaters) {
-				assert.equal(err, null);
-				done();
-			});
+      p.findTheaters('Montreal', {}, function(err, theaters) {
+        assert.equal(err, null);
+        done();
+      });
     })
 
     it('should find theaters by lat/long', function(done){
-    	p.findTheaters('Montreal', {}, function(err, theaters) {
-				assert.equal(err, null);
-				done();
-			});
+      p.findTheaters('Montreal', {}, function(err, theaters) {
+        assert.equal(err, null);
+        done();
+      });
     })
 
     it('should return requestjs error', function(done){
-    	p.GOOGLE_ENDPOINT = 'abc'; // Override endpoint
-    	p.findTheaters('Montreal', {}, function(err, theaters) {
-				assert.notEqual(err, null);
-				done();
-			});
+      p.GOOGLE_ENDPOINT = 'abc'; // Override endpoint
+      p.findTheaters('Montreal', {}, function(err, theaters) {
+        assert.notEqual(err, null);
+        done();
+      });
     })
 
     it('should return a 404 error', function(done){
-    	p.GOOGLE_ENDPOINT = 'http://httpstat.us/404' // Override endpoint
-    	p.findTheaters('Montreal', {}, function(err, theaters) {
-				assert.equal(err, 404);
-				done();
-			});
+      p.GOOGLE_ENDPOINT = 'http://httpstat.us/404' // Override endpoint
+      p.findTheaters('Montreal', {}, function(err, theaters) {
+        assert.equal(err, 404);
+        done();
+      });
     })
 
     it('should return theaters more quickly the second time', function(done){
@@ -81,34 +73,27 @@ describe('Projection', function(){
     })
   })
 
-// Expected JSON format
-// title: "Avengers"
-// theaters: [
-// 	{ name: "...", showtimes: [7:00, 8:00] },
-// 	{ name: "...", showtimes: [7:00, 8:00] }
-// ]
-
-  describe('#findMovie()', function(){
+  describe('.findMovie()', function(){
     it('should find a movie\'s showtimes', function(done){
-    	p.findMovie('Sherbrooke', 'Mad Max', {}, function(err, movie){
-    		done();
-    	});
+      p.findMovie('Sherbrooke', 'Mad Max', {}, function(err, movie){
+        done();
+      });
     })
 
     it('should return requestjs error', function(done){
-    	p.GOOGLE_ENDPOINT = 'abc'; // Override endpoint
-    	p.findMovie('Montreal', 'Mad Max', {}, function(err, theaters) {
-				assert.notEqual(err, null);
-				done();
-			});
+      p.GOOGLE_ENDPOINT = 'abc'; // Override endpoint
+      p.findMovie('Montreal', 'Mad Max', {}, function(err, theaters) {
+        assert.notEqual(err, null);
+        done();
+      });
     })
 
     it('should return a 404 error', function(done){
-    	p.GOOGLE_ENDPOINT = 'http://httpstat.us/404' // Override endpoint
-    	p.findMovie('Montreal', 'Mad Max', {}, function(err, theaters) {
-				assert.equal(err, 404);
-				done();
-			});
+      p.GOOGLE_ENDPOINT = 'http://httpstat.us/404' // Override endpoint
+      p.findMovie('Montreal', 'Mad Max', {}, function(err, theaters) {
+        assert.equal(err, 404);
+        done();
+      });
     })
 
     it('should return a movie more quickly the second time', function(done){
