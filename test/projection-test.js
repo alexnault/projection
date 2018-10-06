3a1abcdc0a8e387ca8dac14848e0864a8cba32da
Alex Nault <nault.alex@gmail.com>
2018-10-05 21:13:19 -0400

    Add eslint

--- test/projection-test.js
+++ test/projection-test.js
@@ -1,22 +1,21 @@
-'use strict';
+const assert = require('assert');
+const projection = require('../lib/projection');
 
-var projection = require('../lib/projection'),
-  assert = require('assert');
 
-var p; // Projection
+let p; // Projection
 
-describe('Projection', function() {
-  beforeEach(function() {
+describe('Projection', () => {
+  beforeEach(() => {
     p = projection();
-  })
+  });
 
-  it('should have the corresponding endpoint', function() {
+  it('should have the corresponding endpoint', () => {
     assert.equal(p.GOOGLE_ENDPOINT, 'http://www.google.com/movies');
-  })
+  });
 
-  describe('.findTheaters()', function(){
-    it('should find theaters by town', function(done) {
-      p.findTheaters('Montreal', {}, function(err, theaters) {
+  describe('.findTheaters()', () => {
+    it('should find theaters by town', (done) => {
+      p.findTheaters('Montreal', {}, (err, theaters) => {
         assert.equal(err, null);
         assert(theaters.length > 0);
         assert(theaters[0].name);
@@ -24,17 +23,17 @@ describe('Projection', function() {
         // TODO test returned content
         done();
       });
-    })
+    });
 
-    it('should find theaters by lat/long', function(done) {
-      p.findTheaters('45.3838273,-71.8958539', {}, function(err, theaters) {
+    it('should find theaters by lat/long', (done) => {
+      p.findTheaters('45.3838273,-71.8958539', {}, (err, theaters) => {
         assert.equal(err, null);
         assert(theaters.length > 0);
         assert(theaters[0].name);
         // TODO test returned content
         done();
       });
-    })
+    });
 
     // it('should find theaters by zipcode', function(done) {
     //   p.findTheaters('Montreal', {}, function(err, theaters) {
@@ -43,47 +42,47 @@ describe('Projection', function() {
     //   });
     // })
 
-    it('should return requestjs error', function(done) {
+    it('should return requestjs error', (done) => {
       p.GOOGLE_ENDPOINT = 'abc';
-      p.findTheaters('Montreal', {}, function(err, theaters) {
+      p.findTheaters('Montreal', {}, (err, theaters) => {
         assert.notEqual(err, null);
         done();
       });
-    })
+    });
 
-    it('should return a 404 error', function(done) {
+    it('should return a 404 error', (done) => {
       p.GOOGLE_ENDPOINT = 'http://httpstat.us/404';
-      p.findTheaters('Montreal', {}, function(err, theaters) {
+      p.findTheaters('Montreal', {}, (err, theaters) => {
         assert.equal(err, 404);
         done();
       });
-    })
+    });
 
-    it('should return an error if no theaters are found', function(done) {
-      p.findTheaters('Innexistant Place', {}, function(err, theaters) {
+    it('should return an error if no theaters are found', (done) => {
+      p.findTheaters('Innexistant Place', {}, (err, theaters) => {
         assert.notEqual(err, null);
         done();
       });
-    })
+    });
 
-    it('should return theaters more quickly the second time', function(done) {
-      var start = new Date().getTime();
-      p.findTheaters('Montreal', {}, function(err, theaters) {
-        var t1 = new Date().getTime() - start;
+    it('should return theaters more quickly the second time', (done) => {
+      let start = new Date().getTime();
+      p.findTheaters('Montreal', {}, (err, theaters) => {
+        const t1 = new Date().getTime() - start;
         start = new Date().getTime();
-        p.findTheaters('Montreal', {}, function(err, theaters) {
-          var t2 = new Date().getTime() - start;
+        p.findTheaters('Montreal', {}, (err, theaters) => {
+          const t2 = new Date().getTime() - start;
           assert(t1 >= t2); // ~500ms to ~10ms
           assert(t2 < 50); // Expect cache to take lesser than 50ms
           done();
         });
       });
-    })
-  })
+    });
+  });
 
-  describe('.findMovie()', function() {
-    it('should find a movie\'s showtimes by town', function(done){
-      p.findMovie('Sherbrooke', 'Captain America: Civil War', {}, function(err, movie){
+  describe('.findMovie()', () => {
+    it('should find a movie\'s showtimes by town', (done) => {
+      p.findMovie('Sherbrooke', 'Captain America: Civil War', {}, (err, movie) => {
         assert.equal(err, null);
         assert(movie);
         assert(movie.theaters.length > 0);
@@ -91,10 +90,10 @@ describe('Projection', function() {
         // TODO test returned content
         done();
       });
-    })
+    });
 
-    it('should find a movie\'s showtimes by lat/long', function(done){
-      p.findMovie('45.3838273,-71.8958539', 'Captain America: Civil War', {}, function(err, movie){
+    it('should find a movie\'s showtimes by lat/long', (done) => {
+      p.findMovie('45.3838273,-71.8958539', 'Captain America: Civil War', {}, (err, movie) => {
         assert.equal(err, null);
         assert(movie);
         assert(movie.theaters.length > 0);
@@ -102,7 +101,7 @@ describe('Projection', function() {
         // TODO test returned content
         done();
       });
-    })
+    });
 
     // it('should find a movie\'s showtimes by zipcode', function(done){
     //   p.findMovie('Sherbrooke', 'Mad Max', {}, function(err, movie){
@@ -111,42 +110,41 @@ describe('Projection', function() {
     //   });
     // })
 
-    it('should return requestjs error', function(done) {
+    it('should return requestjs error', (done) => {
       p.GOOGLE_ENDPOINT = 'abc';
-      p.findMovie('Montreal', 'Mad Max', {}, function(err, theaters) {
+      p.findMovie('Montreal', 'Mad Max', {}, (err, theaters) => {
         assert.notEqual(err, null);
         done();
       });
-    })
+    });
 
-    it('should return a 404 error', function(done) {
+    it('should return a 404 error', (done) => {
       p.GOOGLE_ENDPOINT = 'http://httpstat.us/404';
-      p.findMovie('Montreal', 'Mad Max', {}, function(err, theaters) {
+      p.findMovie('Montreal', 'Mad Max', {}, (err, theaters) => {
         assert.equal(err, 404);
         done();
       });
-    })
+    });
 
-    it('should return an error if the movie is not found', function(done) {
-      p.findMovie('Montreal', 'Random movie name that does not exist', {}, function(err, theaters) {
+    it('should return an error if the movie is not found', (done) => {
+      p.findMovie('Montreal', 'Random movie name that does not exist', {}, (err, theaters) => {
         assert.notEqual(err, null);
         done();
       });
-    })
+    });
 
-    it('should return a movie more quickly the second time', function(done) {
-      var start = new Date().getTime();
-      p.findMovie('Montreal', 'Captain America: Civil War', {}, function(err, theaters) {
-        var t1 = new Date().getTime() - start;
+    it('should return a movie more quickly the second time', (done) => {
+      let start = new Date().getTime();
+      p.findMovie('Montreal', 'Captain America: Civil War', {}, (err, theaters) => {
+        const t1 = new Date().getTime() - start;
         start = new Date().getTime();
-        p.findMovie('Montreal', 'Captain America: Civil War', {}, function(err, theaters) {
-          var t2 = new Date().getTime() - start;
+        p.findMovie('Montreal', 'Captain America: Civil War', {}, (err, theaters) => {
+          const t2 = new Date().getTime() - start;
           assert(t1 >= t2); // ~500ms to ~10ms
           assert(t2 < 100); // Expect cache to take lesser than 100ms
           done();
         });
       });
-    })
-  })
-
-})
+    });
+  });
+});
